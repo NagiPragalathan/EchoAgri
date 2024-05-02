7513549d16001fd2ff17537fa0e076acda47cb75
NagiPragalathan <nagipragalathan@gmail.com>
2024-05-02 12:51:39 +0530

    sample commit

--- EchoAgri/settings.py
+++ EchoAgri/settings.py
@@ -117,8 +117,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
