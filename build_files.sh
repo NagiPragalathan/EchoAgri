db031eb14c125f2548ec7ec803934571ee2cce25
NagiPragalathan <nagipragalathan@gmail.com>
2024-05-02 13:03:24 +0530

    build file updated

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
-# build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Run Django collectstatic command
+python3.9 manage.py collectstatic --noinput
